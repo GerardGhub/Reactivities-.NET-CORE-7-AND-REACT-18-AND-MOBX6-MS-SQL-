a3f73dea5d6fc01d54623894f29f2c97941cb92b
gerard singian <gerard.singian@outlook.com>
2021-12-15 18:32:03 +0800

    Reviewing the project files and startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -44,7 +44,7 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
