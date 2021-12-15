81889eb46d70c8c35cc915a52f0b9a3c177946d7
gerard singian <gerard.singian@outlook.com>
2021-12-15 18:49:17 +0800

    Creating Domain Entity

---
+++ Domain/Activity.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Activity
+    {
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+    public string Category {get; set;}
+    public string City { get; set; }
+    public string Venue { get; set; }
+    }
+}
