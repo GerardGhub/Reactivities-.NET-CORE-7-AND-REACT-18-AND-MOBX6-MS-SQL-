2e06b9ee2725675cd269e10e238e7c34eef073a4
gerard singian <gerard.singian@outlook.com>
2021-12-21 17:50:19 +0800

    Semantic UI  React

--- API/Startup.cs
+++ API/Startup.cs
@@ -44,6 +44,13 @@ namespace API
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +66,7 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ducks } from './demo';
-import DuckItem from './DuckItem';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {ducks.map(duck => (
-      <DuckItem duck={duck} key={duck.name}/>
-        ))}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+
+      <List>
+      {activities.map((activity : any) => (
+      <List.Item key={activity.id}>
+      {activity.title}
+      </List.Item>
+      ))}
+      </List>
+
+   
     </div>
   );
 }
