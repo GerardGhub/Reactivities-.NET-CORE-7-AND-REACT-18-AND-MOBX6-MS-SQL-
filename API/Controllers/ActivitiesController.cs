4bcd93a4aa9306ee824bee2cd86ef9af237ec251
gerard singian <gerard.singian@outlook.com>
2021-12-23 22:27:35 +0800

    Adding a Create Handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -18,5 +18,11 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query{Id = id});
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+        }
     }
 }
