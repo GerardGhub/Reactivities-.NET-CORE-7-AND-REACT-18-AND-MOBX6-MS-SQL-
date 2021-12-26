78ef7b6bce6db515d70bb76a3124f5915fd1b1e8
gerard singian <gerard.singian@outlook.com>
2021-12-26 21:45:33 +0800

    Handle and create edit Submission Takla

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -5,9 +5,10 @@ import { Activity } from "../../../app/models/activity";
 interface Props {
     activity: Activity | undefined;
     closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
 }
 
-export default function ActivityForm({ activity: selectedActivity, closeForm }: Props) {
+export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit }: Props) {
 
     const initialState = selectedActivity ?? {
         id: '',
@@ -23,7 +24,8 @@ export default function ActivityForm({ activity: selectedActivity, closeForm }: 
     const [activity, setActivity] = useState(initialState);
 
     function handleSubmit() {
-        console.log(activity);
+        // console.log(activity);
+        createOrEdit(activity);
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
--- client-app/src/features/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/dashboard/ActivityDashboard.tsx
@@ -15,10 +15,12 @@ interface Props {
     editMode: boolean;
     openForm: (id: string) => void;
     closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+
 }
 
 export default function ActivityDashboard({ activities, selectedActivity,
-     selectActivity, cancelSelectActivity, editMode, openForm, closeForm }: Props) {
+     selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -32,7 +34,7 @@ export default function ActivityDashboard({ activities, selectedActivity,
                  openForm={openForm}
                  />}
                  {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} />}
+                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
             </Grid.Column>
         </Grid>
     )
