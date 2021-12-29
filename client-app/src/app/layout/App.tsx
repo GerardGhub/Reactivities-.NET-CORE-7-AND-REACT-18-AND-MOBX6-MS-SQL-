d8cf7f444c8b93f9810832b54f09d9014e031fbd
gerard singian <gerard.singian@outlook.com>
2021-12-30 00:37:26 +0800

    refactoring the app using MOBX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -15,20 +15,12 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      })
-      setActivities(activities);
-      setLoading(false);
-    })
-  }, [])
+activityStore.loadActivities();
+  }, [activityStore])
 
   function handleSelectActivity(id: string) {
     setSelectedActivity(activities.find(x => x.id === id))
@@ -78,16 +70,16 @@ function handleDeleteActivity(id: string) {
  
 }
 
-if (loading) return <LoadingComponent content = 'Loading app' />
+if (activityStore.loadingInitial) return <LoadingComponent content = 'Loading app' />
 
   return (
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
-        <h2>{activityStore.title}</h2>
-        <Button content='Add exclamation!' positive onClick={activityStore.setTitle} />
+        {/* <h2>{activityStore.title}</h2>
+        <Button content='Add exclamation!' positive onClick={activityStore.setTitle} /> */}
         <ActivityDashboard
-          activities={activities}
+          activities={activityStore.activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectActivity}
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,14 +1,31 @@
 import { makeAutoObservable, makeObservable, observable } from "mobx";
+import agent from "../api/agent";
+import { Activity } from "../models/activity";
 
 export default class ActivityStore {
-    title = 'Hello from Mobx!';
+   activities: Activity[] = [];
+   selectedActivity: Activity | null = null;
+   editMode = false;
+   loading = false;
+   loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
     }
 
-    setTitle = () => {
-        this.title = this.title + '!';
+    loadActivities = async () => {
+        this.loadingInitial = true;
+        try {
+            const activities = await agent.Activities.list();
+            activities.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
+              })
+              this.loadingInitial = false;
+        } catch (error) {
+            console.log(error);
+            this.loadingInitial = false;
+        }
     }
 
 }
