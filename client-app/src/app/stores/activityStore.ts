eaca3876a16be6b9a33224281a6812a1c747ad9b
gerard singian <gerard.singian@outlook.com>
2021-12-30 12:03:32 +0800

    MobX strict mode fixer

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,31 +1,36 @@
-import { makeAutoObservable, makeObservable, observable } from "mobx";
+import { makeAutoObservable} from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
 
 export default class ActivityStore {
-   activities: Activity[] = [];
-   selectedActivity: Activity | null = null;
-   editMode = false;
-   loading = false;
-   loadingInitial = false;
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
     }
 
     loadActivities = async () => {
-        this.loadingInitial = true;
+        this.loadingInitial =(true);
         try {
             const activities = await agent.Activities.list();
-            activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activities.push(activity);
-              })
-              this.loadingInitial = false;
+        
+                activities.forEach(activity => {
+                    activity.date = activity.date.split('T')[0];
+                    this.activities.push(activity);
+                })
+        this.setLoadingInitial(false);
+
         } catch (error) {
-            console.log(error);
-            this.loadingInitial = false;
+            console.log(error);      
+            this.setLoadingInitial(false);
         }
     }
 
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
+    }
 }
