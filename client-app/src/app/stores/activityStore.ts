4b4515e708585e29681cd4d38085ca91f7282271
gerard singian <gerard.singian@outlook.com>
2022-01-02 23:33:04 +0800

    Redirect after Submission

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
-import { v4 as uuid } from 'uuid';
 
 export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
@@ -81,7 +80,6 @@ export default class ActivityStore {
 
     createActivity = async (activity: Activity) => {
         this.loading = true;
-        activity.id = uuid();
         try {
             await agent.Activities.create(activity)
             runInAction(() => {
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,14 +1,15 @@
 import { observer } from "mobx-react-lite";
 import React, { ChangeEvent, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import { v4 as uuid } from 'uuid';
 
 
 
 export default observer(function ActivityForm() {
-
+    const history = useHistory();
     const { activityStore } = useStore();
     const { createActivity, updateActivity, 
         loading, loadActivity, loadingInitial } = activityStore;
@@ -31,7 +32,15 @@ export default observer(function ActivityForm() {
 
 
     function handleSubmit() {
-        activity.id ? updateActivity(activity) : createActivity(activity);
+       if(activity.id.length === 0 ){
+           let newActivity = {
+               ...activity,
+               id: uuid()
+           };
+           createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`))
+       } else {
+           updateActivity(activity).then(() => history.push(`/activities/${activity.id}`))
+       }
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
