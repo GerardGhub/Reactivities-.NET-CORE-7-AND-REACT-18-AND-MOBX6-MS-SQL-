f41310bcee28dcbaae1425b48bdd055af7909988
gerard singian <gerard.singian@outlook.com>
2022-01-04 22:51:55 +0800

    Styling the List Items

--- client-app/src/features/dashboard/ActivityList.tsx
+++ client-app/src/features/dashboard/ActivityList.tsx
@@ -1,25 +1,34 @@
-import React, { SyntheticEvent, useState } from "react";
+import { group } from "console";
+import React, { Fragment, SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
-import { Item, Segment } from "semantic-ui-react";
+import { Header, Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import ActivityListItem from "./ActivityListItem";
 
 
 export default function ActivityList() {
 
-    const {activityStore} = useStore();
-    const {activitiesByDate} = activityStore;
-
-
-
+    const { activityStore } = useStore();
+    const { groupedActivities } = activityStore;
 
     return (
-        <Segment>
-            <Item.Group divided>
-                {activitiesByDate.map(activity => (
-                    <ActivityListItem  key={activity.id} activity={activity}/>
-                ))}
-            </Item.Group>
-        </Segment>
+        <>
+            {groupedActivities.map(([group, activities]) => (
+                <Fragment key={group}>
+                    <Header sub color='teal'>
+                        {group}
+                    </Header>
+
+                    {activities.map(activity => (
+                        <ActivityListItem key={activity.id} activity={activity} />
+                    ))}
+
+                </Fragment>
+            ))}
+
+        </>
+
+
+
     )
 }
--- client-app/src/features/dashboard/ActivityListItem.tsx
+++ client-app/src/features/dashboard/ActivityListItem.tsx
@@ -1,6 +1,6 @@
 import React, { SyntheticEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Item, Label } from 'semantic-ui-react';
+import { Button, Icon, Item, ItemGroup, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../app/models/activity';
 import { useStore } from '../../app/stores/store';
 
@@ -8,11 +8,11 @@ interface Props {
     activity: Activity
 }
 
-export default function ActivityListItem({activity}: Props){
+export default function ActivityListItem({ activity }: Props) {
 
 
-    const {activityStore} = useStore();
-    const {deleteActivity, loading } = activityStore;
+    const { activityStore } = useStore();
+    const { deleteActivity, loading } = activityStore;
 
     const [target, setTarget] = useState('');
 
@@ -21,31 +21,36 @@ export default function ActivityListItem({activity}: Props){
         deleteActivity(id);
     }
 
-    return(
-        <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>
-                                {activity.title}
-                            </Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>
-                                {activity.description}
-                                </div>
-                                <div>{activity.city}, {activity.venue}</div>
-                                </Item.Description>
-                                <Item.Extra>
-                                    <Button as={Link} to={`/activities/${activity.id}`} floated="right" content='View' color='blue' />
-                                    <Button 
-                                    name={activity.id}
-                                    loading={loading && target === activity.id} 
-                                    onClick={(e) => handleActivityDelete(e, activity.id)} 
-                                    floated="right" 
-                                    content='Delete' 
-                                    color='red' />
-                                    <Label basic content={activity.category} />
-                                </Item.Extra>
-                        </Item.Content>
-                    </Item>
+    return (
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item.Image size='tiny' circular src='/assets/user.png' />
+                    <Item.Content>
+                        <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
+                        <Item.Description>Hosted by Bob</Item.Description>
+                    </Item.Content>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {activity.date}
+                    <Icon name='marker' /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='teal'
+                    floated='right'
+                    content='View'
+                />
+            </Segment>
+        </Segment.Group>
     )
 }
