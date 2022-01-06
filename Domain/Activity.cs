ae7b5bf4b13333f91951a9a98bff15f4d073fbf6
gerard singian <gerard.singian@outlook.com>
2022-01-06 23:56:18 +0800

    Validation Data Annotations

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Domain
     public class Activity
     {
     public Guid Id { get; set; }
+    [Required]
     public string Title { get; set; }
     public DateTime Date { get; set; }
     public string Description { get; set; }
