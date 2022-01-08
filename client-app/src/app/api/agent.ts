6ce648f1f8a361d03c6ec2e350dfb9abcb37baec
gerard singian <gerard.singian@outlook.com>
2022-01-08 14:43:30 +0800

    Handling validation on invalid GUID ID

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -19,9 +19,15 @@ axios.interceptors.response.use(async response => {
     return response;
 
 }, (error: AxiosError) => {
-    const { data, status } = error.response!;
+    const { data, status, config } = error.response!;
     switch (status) {
         case 400:
+            if(typeof data == 'string') {
+                toast.error(data);
+            }
+            if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
+                history.push('/not-found');
+            }
             if(data.errors) {
                 const modalStateErrors = [];
                 for (const key in data.errors){
@@ -32,8 +38,6 @@ axios.interceptors.response.use(async response => {
                 }
                 throw modalStateErrors.flat();
                 
-            }else {
-                toast.error(data);
             }
             break;
         case 401:
--- client-app/src/features/errors/TestError.tsx
+++ client-app/src/features/errors/TestError.tsx
@@ -24,7 +24,7 @@ export default function TestErrors() {
     }
 
     function handleBadGuid() {
-        axios.get(baseUrl + 'activities/notaguid').catch(err => console.log(err.response));
+        axios.get(baseUrl + 'activities/notaguid').catch(err => console.log(err));
     }
 
     function handleValidationError() {
