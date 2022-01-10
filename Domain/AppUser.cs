8a925d21616adeb4c22d84bf5f4004d574b0281e
gerard singian <gerard.singian@outlook.com>
2022-01-11 00:01:17 +0800

    Adding User Entity Back End

---
+++ Domain/AppUser.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain
+{
+    public class AppUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
