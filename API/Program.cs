45c16c8cf740b369a956fb25daa1998c54a669de
gerard singian <gerard.singian@outlook.com>
2022-01-12 00:12:05 +0800

    Seeding Users

--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -24,8 +26,9 @@ namespace API
             try
             {
                  var context = services.GetRequiredService<DataContext>();
+                 var userManager = services.GetRequiredService<UserManager<AppUser>>();
                  await context.Database.MigrateAsync();
-                 await Seed.SeedData(context);
+                 await Seed.SeedData(context, userManager);
             }
             catch (Exception ex)
             {
