e6a4bca73e07e749c1c810efe3ea96ba9ab70e82
gerard singian <gerard.singian@outlook.com>
2022-01-11 23:29:28 +0800

    Identity DbContext

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
