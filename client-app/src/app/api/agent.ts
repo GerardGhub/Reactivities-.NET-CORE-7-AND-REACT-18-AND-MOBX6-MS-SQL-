b603750584cea3de48c310e9add657128102118c
gerard singian <gerard.singian@outlook.com>
2022-01-14 23:04:13 +0800

    creating interfaces and methods

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { Activity } from "../models/activity";
+import { User, UserFormValues } from "../models/user";
 import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
@@ -71,7 +72,15 @@ const Activities = {
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
+const Account = {
+    current: () => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+    register: (user: UserFormValues)  => requests.post<User>('/account/register', user)
+}
+
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 export default agent;
