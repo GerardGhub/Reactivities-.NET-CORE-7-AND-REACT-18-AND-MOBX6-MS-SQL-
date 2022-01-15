7eb3f8859f346adee19d39a28812a65396ae45b8
gerard singian <gerard.singian@outlook.com>
2022-01-15 19:36:30 +0800

    Homepage Navbar Front end

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,20 +1,32 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
 
-export default function HomePage(){
-    return (
-      <Segment inverted textAlign='center' vertical className='masthead'>
-          <Container text>
-            <Header as='h1' inverted>
-            <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}}/>
-            Reactivities
-            </Header>
+export default observer(function HomePage() {
+  const { userStore } = useStore();
+  return (
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container text>
+        <Header as='h1' inverted>
+          <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
+          Reactivities
+        </Header>
+        {userStore.isLoggedIn ? (
+          <>
             <Header as='h2' inverted content='Welcome to Reactivities' />
-            <Button as={Link} to='/login' size='huge' inverted>
-              Login!
+            <Button as={Link} to='/activities' size='huge' inverted>
+              Go to Activities!
             </Button>
-          </Container>
-      </Segment>
-    )
-}
+          </>
+
+        ) : (
+          <Button as={Link} to='/login' size='huge' inverted>
+            Login!
+          </Button>
+        )}
+      </Container>
+    </Segment>
+  )
+})
