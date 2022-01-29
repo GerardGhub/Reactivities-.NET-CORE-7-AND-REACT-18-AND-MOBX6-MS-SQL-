9c57cfe65eea2037d223ae5a4fa2d2aff637306d
gerard singian <gerard.singian@outlook.com>
2022-01-29 23:01:33 +0800

    Adding Photo Entity

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -12,5 +12,6 @@ namespace Domain
 
         public string Bio { get; set; }
         public ICollection<ActivityAttendee> Activities {get; set;}
+        public ICollection<Photo> Photos {get; set;}
     }
 }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -16,6 +16,7 @@ namespace Persistence
 
         public DbSet<Activity> Activities {get; set;}
         public DbSet<ActivityAttendee> ActivityAttendees {get; set;}
+        public DbSet<Photo> Photos {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
