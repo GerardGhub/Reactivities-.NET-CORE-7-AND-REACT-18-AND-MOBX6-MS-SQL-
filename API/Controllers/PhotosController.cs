1c4d183645142f9fd101f3038de7da27ea8c083b
gerard singian <gerard.singian@outlook.com>
2022-01-31 23:37:03 +0800

    Meitundu naku

---
+++ API/Controllers/PhotosController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Photos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class PhotosController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] Add.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            return  HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
+        }
+
+    }
+}
