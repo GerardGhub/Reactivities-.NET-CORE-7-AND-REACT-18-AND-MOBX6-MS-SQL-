e0c5329d7bbf531e23e18a5ef1ada393bdeafc56
gerard singian <gerard.singian@outlook.com>
2022-02-06 00:41:39 +0800

    Adding a profile header front end

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import ProfileHeader from './ProfileHeader'
 export default function ProfilePage() {
     return (
-        <h1>Profile</h1>
+        <Grid>
+            <Grid.Column width={16}>
+                <ProfileHeader />
+            </Grid.Column>
+        </Grid>
+
     )
 }
