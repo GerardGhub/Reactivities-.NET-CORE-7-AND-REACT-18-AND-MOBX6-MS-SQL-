89d06532e8d3958e1be2f2e725eb7503d8dfae83
gerard singian <gerard.singian@outlook.com>
2022-02-06 01:26:27 +0800

    Profile Image Tulog Na

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import { observer } from 'mobx-react-lite'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { Grid } from 'semantic-ui-react'
+import LoadingComponent from '../../app/layout/LoadingComponent'
+import { useStore } from '../../app/stores/store'
 import ProfileContent from './ProfileContent'
 import ProfileHeader from './ProfileHeader'
-export default function ProfilePage() {
+
+
+
+export default observer(function ProfilePage() {
+    const { username } = useParams<{ username: string }>();
+    const { profileStore } = useStore();
+    const { loadingProfile, loadProfile, profile } = profileStore;
+
+
+    useEffect(() => {
+        loadProfile(username);
+    }, [loadProfile, username])
+
+
+    if (loadingProfile) return <LoadingComponent content='Loading profile...' />
+
     return (
         <Grid>
             <Grid.Column width={16}>
-                <ProfileHeader />
+                {profile &&
+                <ProfileHeader profile={profile}/> }
                 <ProfileContent />
             </Grid.Column>
         </Grid>
 
     )
-}
+})
