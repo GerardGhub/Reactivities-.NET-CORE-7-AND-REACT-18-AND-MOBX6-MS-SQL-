1d84721ec3f7d34966d25f2ad858cd3fed5993cb
gerard singian <gerard.singian@outlook.com>
2022-02-06 15:11:42 +0800

    Adding a react - cropper

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
-import { Grid, Header, Image } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Button, Grid, Header, Image } from 'semantic-ui-react';
+import PhotoWidgetCropper from './PhotoWidgetCropper';
 import PhotoWidgetDropzone from './PhotoWidgetDropzone';
+
 
 export default function PhotoUploadWidget() {
 
     const [files, setFiles] = useState<any>([]);
+    const [cropper, setCropper] = useState<Cropper>();
+
+
+    function onCrop() {
+        if (cropper) {
+            cropper.getCroppedCanvas().toBlob(blob => console.log(blob));
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            files.forEach((file: any) => URL.revokeObjectURL(file.preview))
+        }
+    }, [files])
 
     return (
         <Grid>
@@ -18,7 +34,7 @@ export default function PhotoUploadWidget() {
             <Grid.Column width={4}>
                 <Header sub color='teal' content='Step 2 - Resize Image' />
                 {files && files.length > 0 && (
-                    <Image src={files[0].preview} />
+                    <PhotoWidgetCropper setCropper={setCropper} imagePreview={files[0].preview} />
                 )}
             </Grid.Column>
 
@@ -26,6 +42,15 @@ export default function PhotoUploadWidget() {
 
             <Grid.Column width={4}>
                 <Header sub color='teal' content='Step 3 - Preview & Upload' />
+                {files && files.length > 0 &&
+                <>
+                    <div className='img-preview' style={{ minHeight: 200, overflow: 'hidden' }} />
+                    <Button.Group widths={2}>
+                        <Button onClick={onCrop} positive icon='check' />
+                        <Button onClick={() => setFiles([])} icon='close' />
+                    </Button.Group>
+                </>}
+
             </Grid.Column>
         </Grid>
     )
--- client-app/src/app/common/imageUpload/PhotoWidgetCropper.tsx
+++ client-app/src/app/common/imageUpload/PhotoWidgetCropper.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { Cropper } from "react-cropper";
+import 'cropperjs/dist/cropper.css';
 
-export default function PhotoWidgetCropper() {
+interface Props {
+    imagePreview: string;
+    setCropper: (cropper: Cropper) => void;
+}
+
+export default function PhotoWidgetCropper({imagePreview, setCropper}: Props) {
     return (
-        <Cropper>
-            
-        </Cropper>
+        <Cropper
+            src={imagePreview}
+            style={{height: 200, width: '100%'}}
+            initialAspectRatio={1}
+            aspectRatio={1}
+            preview='.img-preview'
+            guides={false}
+            viewMode={1}
+            autoCropArea={1}
+            background={false}
+            onInitialized={cropper => setCropper(cropper)}
+        />
     )
 }
