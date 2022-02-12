4a7324bfa5b1d0e0a81851f795bc8a8994c759fc
gerard singian <gerard.singian@outlook.com>
2022-02-12 23:22:20 +0800

    Adding Create Handler

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -16,7 +16,7 @@ namespace Application.Core
             CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>()
             .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees
-            .FirstOrDefault(x => x.IsHost).AppUser.UserName));
+            .FirstOrDefault(x => x.IsHost).AppUser.UserName));  
 
             CreateMap<ActivityAttendee, AttendeeDto>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
@@ -24,7 +24,7 @@ namespace Application.Core
             .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
              .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<AppUser, Profiles.Profile>()
-            .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url)); 4
+            .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url)); 
             CreateMap<Comment, CommentDto>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
             .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
