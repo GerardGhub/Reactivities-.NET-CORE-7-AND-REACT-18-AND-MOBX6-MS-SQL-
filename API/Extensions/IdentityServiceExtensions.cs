198a466af68e9cd759a653e692cce49d0abd23c8
gerard singian <gerard.singian@outlook.com>
2022-02-13 00:15:39 +0800

    Authenticating Signal R backend

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -44,6 +44,20 @@ namespace API.Extensions
 
                 };
 
+                opt.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if(!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+
             });
 
 
