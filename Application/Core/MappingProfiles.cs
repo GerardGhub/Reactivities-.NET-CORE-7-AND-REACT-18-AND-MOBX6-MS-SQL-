5d0941f741bb07255805a99218674802ccfee9ec
gerard singian <gerard.singian@outlook.com>
2022-02-14 23:39:51 +0800

    update the profile class

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -16,7 +16,7 @@ namespace Application.Core
             CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>()
             .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees
-            .FirstOrDefault(x => x.IsHost).AppUser.UserName));  
+            .FirstOrDefault(x => x.IsHost).AppUser.UserName));
 
             CreateMap<ActivityAttendee, AttendeeDto>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
@@ -24,7 +24,9 @@ namespace Application.Core
             .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
              .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<AppUser, Profiles.Profile>()
-            .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url)); 
+            .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url))
+            .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+            .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count));
             CreateMap<Comment, CommentDto>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
             .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -9,10 +9,14 @@ namespace Application.Profiles
     public class Profile
     {
         public string Username { get; set; }
-        public string DisplayName {get; set;}
-        public string Bio {get; set;}
-        public string Image {get; set;}
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
 
-        public ICollection<Photo> Photos {get; set;}
+        public bool Following { get; set; }
+        public int FollowersCount { get; set; }
+        public int FollowingCount { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
     }
 }
