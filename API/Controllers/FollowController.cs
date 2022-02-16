95829fac7439291c0717b26b320b87e70626a117
gerard singian <gerard.singian@outlook.com>
2022-02-16 22:31:30 +0800

    Return a list of followers

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -14,5 +14,13 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(new FollowToggle.Command{TargetUsername = username}));
         }
+
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetFollowings(string username, string predicate)
+        {
+            return HandleResult(await Mediator.Send(new List.Query{Username = username,
+            Predicate = predicate}));
+        }
     }
 }
