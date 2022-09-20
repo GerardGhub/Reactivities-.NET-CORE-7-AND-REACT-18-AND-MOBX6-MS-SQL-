804926b54cbb7c1f1448add043871f2322e647ee
GerardGhub <singian28@gmail.com>
2022-09-20 22:40:41 +0800

    Configuring API Endpoint for the Facebook Login sa fucking AccountController

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace API.Controllers
 {
@@ -21,14 +22,21 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly TokenService _tokenService;
+        private readonly IConfiguration _config;
+        private readonly HttpClient _httpClient;
 
         public AccountController(UserManager<AppUser> userManager,
-        SignInManager<AppUser> signInManager, TokenService tokenService)
+        SignInManager<AppUser> signInManager, TokenService tokenService,
+         IConfiguration config)
         {
+            this._config = config;
             _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
-
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new System.Uri("https://graph.facebook.com")
+            };
 
         }
         [HttpPost("login")]
@@ -43,7 +51,7 @@ namespace API.Controllers
 
             if (result.Succeeded)
             {
-                 return CreateUserObject(user);
+                return CreateUserObject(user);
             }
             return Unauthorized();
         }
@@ -89,9 +97,31 @@ namespace API.Controllers
             .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
 
             return CreateUserObject(user);
-
-
         }
+
+
+        public async Task<ActionResult<UserDto>> FacebookLogin(string accessToken)
+        {
+            var fbVerifyKeys = _config["Facebook:AppId"] + "|" + _config["Facebook:AppSecret"];
+
+            var verifyToken = await _httpClient
+            .GetAsync($"debug_token?input_token={accessToken}&access_token={fbVerifyKeys}");
+
+            if (!verifyToken.IsSuccessStatusCode) return Unauthorized();
+
+            var fbUrl = $"me?access_token={accessToken}&fields=name,email,picture.width(100).height(100)";
+
+            var response = await _httpClient.GetAsync(fbUrl);
+
+            if (!response.IsSuccessStatusCode) return Unauthorized();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var fbInfo = JsonConvert.DeserializeObject<dynamic>(content);
+
+            return new UserDto();
+        }
+
 
         private UserDto CreateUserObject(AppUser user)
         {
