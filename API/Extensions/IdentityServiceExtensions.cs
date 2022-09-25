b367f4be44b163d8a0a683789fb3bd09e9037e74
GerardGhub <singian28@gmail.com>
2022-09-25 12:06:17 +0800

    Adding Refresh Token and updating Jwt Token Parameter add package rimraf at front end

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -40,7 +40,9 @@ namespace API.Extensions
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = key,
                     ValidateIssuer = false,
-                    ValidateAudience = false
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
 
                 };
 
