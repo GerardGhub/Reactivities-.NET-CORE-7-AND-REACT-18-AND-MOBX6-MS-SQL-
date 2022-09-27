ae53205ba8acf290edf61d578cdcb2217b497f33
GerardGhub <singian28@gmail.com>
2022-09-27 23:42:45 +0800

    migrating and removing startup

---
+++ API/GlobalUsings.cs
@@ -0,0 +1,25 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using API.Extensions;
+global using API.Middleware;
+global using API.SignalR;
+global using Domain;
+global using FluentValidation.AspNetCore;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Mvc.Authorization;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Persistence;
+
+
+
+global using System.Security.Claims;
+global using API.DTOs;
+global using API.Services;
+global using Microsoft.AspNetCore.Mvc;
