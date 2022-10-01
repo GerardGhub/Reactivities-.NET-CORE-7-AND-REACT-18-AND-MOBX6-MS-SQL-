c889abace5d8eff05d1739aee37898f324bdb46a
GerardGhub <singian28@gmail.com>
2022-10-01 15:23:54 +0800

    Add Content Security Policies

--- API/Program.cs
+++ API/Program.cs
@@ -25,14 +25,14 @@ app.UseXContentTypeOptions();
 app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
-app.UseCspReportOnly(opt => opt
+app.UseCsp(opt => opt
 .BlockAllMixedContent()
-.StyleSources(s => s.Self())
-.FontSources(s => s.Self())
+.StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "https://cdnjs.cloudflare.com"))
+.FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
 .FormActions(s => s.Self())
 .FrameAncestors(s => s.Self())
-.ImageSources(s => s.Self())
-.ScriptSources(s => s.Self())
+.ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "https://www.facebook.com"))
+.ScriptSources(s => s.Self().CustomSources("sha256-xf9uasCuSmsdg8wWbtDMx7pqLTf5pwpWU/rfDrij3Jc=").CustomSources("https://connect.facebook.net"))
 );
 
 if (app.Environment.IsDevelopment())
@@ -40,6 +40,14 @@ if (app.Environment.IsDevelopment())
 
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+}
+else
+{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 // app.UseHttpsRedirection();
