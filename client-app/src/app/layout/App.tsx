fa965d707688a847f698d1e77020b1ccbd00edbd
GerardGhub <singian28@gmail.com>
2022-12-27 22:34:35 +0800

    Remove Fb Login Status

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,15 +1,13 @@
-import React, { useEffect } from 'react';
-import { Container } from 'semantic-ui-react';
-import NavBar from './NavBar';
-import { observer } from 'mobx-react-lite';
-import { Outlet, useLocation } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { useStore } from '../stores/store';
-import LoadingComponent from './LoadingComponent';
-import ModalContainer from '../common/modals/ModalContainer';
-import HomePage from '../../features/home/HomePage';
-
-
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { Container } from "semantic-ui-react";
+import HomePage from "../../features/home/HomePage";
+import ModalContainer from "../common/modals/ModalContainer";
+import { useStore } from "../stores/store";
+import LoadingComponent from "./LoadingComponent";
+import NavBar from "./NavBar";
 
 
 function App() {
@@ -21,7 +19,6 @@ function App() {
       userStore.getUser().finally(() => commonStore.setAppLoaded());
     }
     else {
-      // userStore.getFacebookLoginStatus().then(() => commonStore.setAppLoaded());
       commonStore.setAppLoaded();
     }
   }, [commonStore, userStore])
@@ -30,8 +27,9 @@ function App() {
 
   return (
     <>
-      <ToastContainer position='bottom-right' hideProgressBar />
+    <ScrollRestoration/>
       <ModalContainer />
+      <ToastContainer position='bottom-right' hideProgressBar />
       {location.pathname === '/' ? <HomePage /> : (
         <>
           <NavBar />
