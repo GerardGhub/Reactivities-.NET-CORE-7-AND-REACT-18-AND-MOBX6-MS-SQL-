b162359d418d5b9a363170f6a8d768eb2892ee0d
GerardGhub <singian28@gmail.com>
2023-01-09 01:33:47 +0800

    Updated Content of activity

--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,10 +1,10 @@
 import { format } from 'date-fns';
 import { observer } from 'mobx-react-lite';
+import React from 'react'
 import { Link } from 'react-router-dom';
 import { Button, Header, Item, Segment, Image, Label } from 'semantic-ui-react'
 import { Activity } from "../../../app/models/activity";
 import { useStore } from '../../../app/stores/store';
-import React from 'react';
 
 const activityImageStyle = {
     filter: 'brightness(30%)'
@@ -30,22 +30,22 @@ export default observer(function ActivityDetailedHeader({ activity }: Props) {
             <Segment basic attached='top' style={{ padding: '0' }}>
                 {activity.isCancelled &&
                     <Label style={{ position: 'absolute', zIndex: 1000, left: -14, top: 20 }}
-                        ribbon color='red' content='Cancelled' />
-                }
+                        ribbon color='red' content='Cancelled' />}
                 <Image src={`/assets/categoryImages/${activity.category}.jpg`} fluid style={activityImageStyle} />
                 <Segment style={activityImageTextStyle} basic>
                     <Item.Group>
                         <Item>
-                            <Item.Content>
+                            <Item.Content style={{color: 'black'}}>
                                 <Header
                                     size='huge'
                                     content={activity.title}
-                                    style={{ color: 'white' }}
+                                    style={{ color: 'black', marginRight: '5px' }}
                                 />
-                                <p>{format(activity.date!, 'dd MMM yyyy')}</p>
-                                <p>
-                                    Hosted by <strong><Link to={`/profiles/${activity.host?.username}`}>{activity.host?.displayName}</Link></strong>
-                                </p>
+                                <span style={{marginRight: '5px'}}>
+                                {format(activity.date!, 'dd MMM yyyy')}
+                               </span>
+                             Hosted by <strong><Link to={`/profiles/${activity.hostUsername}`}>{activity.hostUsername}</Link></strong>
+                           
                             </Item.Content>
                         </Item>
                     </Item.Group>
@@ -58,31 +58,29 @@ export default observer(function ActivityDetailedHeader({ activity }: Props) {
                             color={activity.isCancelled ? 'green' : 'red'}
                             floated='left'
                             basic
-                            content={activity.isCancelled ? 'Re-activate Activity' : 'Cancel Activity'}
+                            content={activity.isCancelled ? 'Re-activate activity' : 'Cancel Activity'}
                             onClick={cancelActivityToggle}
                             loading={loading}
                         />
-                        <Button as={Link}
-                            disabled={activity.isCancelled}
+                        <Button
+                            as={Link}
                             to={`/manage/${activity.id}`}
                             color='orange'
-                            floated='right'>
+                            floated='right'
+                            disabled={activity.isCancelled}
+                        >
                             Manage Event
                         </Button>
-
                     </>
 
                 ) : activity.isGoing ? (
-                    <Button loading={loading} onClick={updateAttendance}>Cancel attendance</Button>
+                    <Button onClick={updateAttendance} 
+                        loading={loading}>Cancel attendance</Button>
                 ) : (
-                    <Button disabled={activity.isCancelled}
-                        loading={loading} onClick={updateAttendance} color='teal'>
-                        Join Activity
-                    </Button>
+                    <Button disabled={activity.isCancelled} onClick={updateAttendance} 
+                        loading={loading} color='teal'>Join Activity</Button>
                 )}
-
             </Segment>
         </Segment.Group>
     )
 })
-
